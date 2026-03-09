29448cbe5a22110c92f74dae690aaa2ad7fff5e3
Kevyn Tapnio <tapniokevynapps@gmail.com>
2026-03-10 06:20:17 +0800

    player animation and facing direction added

--- player.gd
+++ player.gd
@@ -1,25 +1,48 @@
 extends CharacterBody2D
 
+const SPEED = 120
+var facing_direction = "down"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _physics_process(delta):
 
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity = direction * SPEED
 	move_and_slide()
+
+	if velocity.x > 0:
+		facing_direction = "right"
+
+		if $AnimatedSprite2D.animation != "walk_right":
+			$AnimatedSprite2D.play("walk_right")
+
+	elif velocity.x < 0:
+		facing_direction = "left"
+
+		if $AnimatedSprite2D.animation != "walk_left":
+			$AnimatedSprite2D.play("walk_left")
+
+	elif velocity.y < 0:
+		facing_direction = "up"
+
+		if $AnimatedSprite2D.animation != "walk_up":
+			$AnimatedSprite2D.play("walk_up")
+
+	elif velocity.y > 0:
+		facing_direction = "down"
+
+		if $AnimatedSprite2D.animation != "walk_down":
+			$AnimatedSprite2D.play("walk_down")
+
+	else:
+		if facing_direction == "right":
+			$AnimatedSprite2D.play("idle_right")
+
+		elif facing_direction == "left":
+			$AnimatedSprite2D.play("idle_left")
+
+		elif facing_direction == "up":
+			$AnimatedSprite2D.play("idle_up")
+
+		elif facing_direction == "down":
+			$AnimatedSprite2D.play("idle_down")
