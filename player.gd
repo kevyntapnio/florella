2ef324a6beee632371c764bfe695cacae1c6e52b
Kevyn Tapnio <tapniokevynapps@gmail.com>
2026-03-10 21:18:24 +0800

    changed camera zoom

--- player.gd
+++ player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 120
 var facing_direction = "down"
 
+@onready var sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,20 +15,20 @@ func _physics_process(delta):
 	if velocity.x > 0:
 		facing_direction = "right"
 
-		if $AnimatedSprite2D.animation != "walk_right":
-			$AnimatedSprite2D.play("walk_right")
+		if sprite.animation != "walk_right":
+			sprite.play("walk_right")
 
 	elif velocity.x < 0:
 		facing_direction = "left"
 
-		if $AnimatedSprite2D.animation != "walk_left":
-			$AnimatedSprite2D.play("walk_left")
+		if sprite.animation != "walk_left":
+			sprite.play("walk_left")
 
 	elif velocity.y < 0:
 		facing_direction = "up"
 
-		if $AnimatedSprite2D.animation != "walk_up":
-			$AnimatedSprite2D.play("walk_up")
+		if sprite.animation != "walk_up":
+			sprite.play("walk_up")
 
 	elif velocity.y > 0:
 		facing_direction = "down"
