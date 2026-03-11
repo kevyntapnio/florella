a319878953917649740ba34c12fb7ffee57421d0
Kevyn Tapnio <tapniokevynapps@gmail.com>
2026-03-11 23:14:47 +0800

    add core interaction system

--- player.gd
+++ player.gd
@@ -5,8 +5,14 @@ var facing_direction = "down"
 
 @onready var sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+var nearby_interactables = []
+var closest_interactable = null
 
+func _physics_process(delta):
+	
+	find_closest_interactable()
+
+	# Movement
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
 	velocity = direction * SPEED
@@ -33,8 +39,35 @@ func _physics_process(delta):
 	elif velocity.y > 0:
 		facing_direction = "down"
 
-		if $AnimatedSprite2D.animation != "walk_down":
-			$AnimatedSprite2D.play("walk_down")
+		if sprite.animation != "walk_down":
+			sprite.play("walk_down")
 
 	else:
-		$AnimatedSprite2D.play("idle_down")
+		sprite.play("idle_down")
+		
+	# Object interaction
+	if Input.is_action_just_pressed("interact"):
+		if closest_interactable:
+			closest_interactable.interact()
+
+
+func _on_interaction_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("interactable"):
+		nearby_interactables.append(body)
+		
+func _on_interaction_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("interactable"):
+		nearby_interactables.erase(body)
+		
+func find_closest_interactable():
+	var closest_object = null
+	var current_smallest_distance = INF
+	
+	for object in nearby_interactables:
+		var distance = global_position.distance_to(object.global_position)
+		
+		if distance < current_smallest_distance:
+			current_smallest_distance = distance
+			closest_object = object
+			
+	closest_interactable = closest_object
